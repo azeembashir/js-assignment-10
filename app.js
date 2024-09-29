bf7980c00b4b6c292b24d891e548666e8082fb4e
Azeem Bashir25 <148478162+azeembashir@users.noreply.github.com>
2024-09-29 11:54:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// task 1
+// let sentence = "hello my name is azeem bashir";
+// let words = sentence.split(" ");
+// let capitalizedWords = [];
+
+// for (let i = 0; i < words.length; i++) {
+//     let word = words[i];
+//     let capitalizedWord = word.slice(0,1).toUpperCase() + word.slice(1);
+//     capitalizedWords.push(capitalizedWord);
+// }
+
+// let result = capitalizedWords.join(" ");
+// console.log(result); 
+// var text = "hello i am saylani mass it training student"
+//  for (let i = 0; i < text.length; i++) {
+//     if (text.slice(i, i+7) ==="saylani") {
+//         console.log("saylani found")
+//         break;
+//     }
+    
+//  }
+//  var word = text.slice(11, 18 )
+// console.log(word)
+
+// task 3
+// var text = "Hello I Am saylani Mass It Training Student"
+// var word = text.indexOf("saylani");
+// console.log(word)
+// for (var i = 0; i < text.length; i++){
+//     if(text.indexOf(i,i + 7) === "saylani"){
+//        break;
+//     }
+// }
+// alert("Word Found")
+
+// task 4
+
+// var text = "hello i am saylani mass it training student"
+// var word = text.replace("saylani", "SMIT" )
+// console.log(word)
